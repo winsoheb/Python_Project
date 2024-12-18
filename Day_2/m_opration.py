351dac7e08161719a3e63b4bfe69b90695dbb215
sohebshaikh <winsoheb@gmail.com>
2024-12-18 20:34:14 +0530

    BMI Calculator

---
+++ Day_2/m_opration.py
@@ -0,0 +1,16 @@
+# print("My age : " + str(123))
+# print(123 + 456)
+# print(7 - 3)
+# print(3 * 2)
+# print(5 / 3)
+# print(5 // 3)
+# print(2 ** 3)
+#
+# # PEMDAS
+# # ()
+# # **
+# # * OR /
+# # + OR -
+
+print(3*3/3+3-3)
+print(3*(3+3)/3-3)
