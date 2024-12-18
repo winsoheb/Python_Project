bcf00502c55d59d51f58327970d6503a9ad2c095
sohebshaikh <winsoheb@gmail.com>
2024-12-18 19:55:46 +0530

    Add type_con

---
+++ Day_2/Typr_conversion.py
@@ -0,0 +1,4 @@
+#Typr conversion
+name = input("Enter Your Name")
+L_name = len(name)
+print("Number of letters in your name : " + str(L_name))
--- Day_2/tipcal.py
+++
@@ -1,4 +0,0 @@
-#Typr conversion
-name = input("Enter Your Name")
-L_name = len(name)
-print("Number of letters in your name : " + str(L_name))
