f81f7f5bb287e0319158b7dabe9b09d265eb2505
sohebshaikh <winsoheb@gmail.com>
2024-12-19 10:35:27 +0530

    BMI Calculator

--- Day_2/m_opration.py
+++ Day_2/m_opration.py
@@ -1,16 +1,36 @@
-# print("My age : " + str(123))
-# print(123 + 456)
-# print(7 - 3)
-# print(3 * 2)
-# print(5 / 3)
-# print(5 // 3)
-# print(2 ** 3)
+# # print("My age : " + str(123))
+# # print(123 + 456)
+# # print(7 - 3)
+# # print(3 * 2)
+# # print(5 / 3)
+# # print(5 // 3)
+# # print(2 ** 3)
+# #
+# # # PEMDAS
+# # # ()
+# # # **
+# # # * OR /
+# # # + OR -
 #
-# # PEMDAS
-# # ()
-# # **
-# # * OR /
-# # + OR -
+# print(3*3/3+3-3)
+# print(3*(3+3)/3-3)
+#
+#Number manipulation
+# bim = 75 / 1.77 ** 2
+# print(bim)
+# print(int(bim))
+# print(round(bim, 2))
 
-print(3*3/3+3-3)
-print(3*(3+3)/3-3)
+#
+# score = 0
+# score += 1
+# print(score)
+
+#F string
+score = 0
+height = 1.8
+is_winning = True
+print(f"Your score is : {score}. \nYour height is : {height}. \nYou are winning is : {is_winning}. ")
+print(6 + 4 / 2 - (1 * 2))
+a = int("5") / int(2.7)
+print(a)
