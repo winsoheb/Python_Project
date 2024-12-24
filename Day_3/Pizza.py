92685e163450e5b79df8f3bb23a58b7797138197
sohebshaikh <winsoheb@gmail.com>
2024-12-24 19:58:29 +0530

    new file

---
+++ Day_3/Pizza.py
@@ -0,0 +1,2 @@
+print("Welcome to Python Pizza Deliveries!")\
+size = input("What size pizza do you want ")
---
+++ Day_3/if_else.py
@@ -0,0 +1,6 @@
+number_to_check = int(input("What is the number your want yo check?"))
+
+if number_to_check % 2  == 0:
+    print("This is Even Number")
+else:
+    print("This is Odd Number")
