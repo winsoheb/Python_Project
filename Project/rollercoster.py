d4592390250c287be17b4ce48302e3be41d5a5ee
sohebshaikh <winsoheb@gmail.com>
2024-12-25 19:12:51 +0530

    new file

--- Project/rollercoster.py
+++ Project/rollercoster.py
@@ -1,24 +1,53 @@
+# print("Welcome to rollercoster Ride")
+#
+# height = int(input("Please enter your height in CM: "))
+# age = int(input("Please enter your Age: "))
+# photo = input("Do you want Photos  Y or N: ").lower()
+#
+# bill = 0
+# if height > 120:
+#     print("You can ride")
+#     if age < 12:
+#         bill = 5
+#     elif age > 12 and age < 19:
+#         bill = 7
+#     elif age > 18 and age < 45:
+#         bill = 12
+#     elif age > 44 and age < 56:
+#         bill = 0
+# else:
+#     print("You can't ride")
+# if photo == "y":
+#     bill += 3
+#     print(f"The total bill is: ${bill}")
+# else:
+#     print(f"The total bill is: ${bill}")
+##################################  New    ###################################################
+
 print("Welcome to rollercoster Ride")
-
 height = int(input("Please enter your height in CM: "))
-age = int(input("Please enter your Age: "))
-photo = input("Do you want Photos  Y or N: ").lower()
-
 bill = 0
-if height > 120:
-    print("You can ride")
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
     if age < 12:
         bill = 5
-    elif age > 12 and age < 19:
+        print("Child tickets are $5.")
+    elif age <= 18:
         bill = 7
-    elif age > 18 and age < 45:
+        print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+            print("Everything is going to be ok. Have a free ride on us!")
+    else:
         bill = 12
-    elif age > 44 and age < 56:
-        bill = 0
+        print("Adult tickets are $12")
+
+    photo = input("Do you want Photos  Y or N: ").lower()
+
+    if photo != "y":
+        pass
+    else:
+        bill += 3
+        print(f"The total bill is: ${bill}")
 else:
-    print("You can't ride")
-if photo == "y":
-    bill += 3
-    print(f"The total bill is: ${bill}")
-else:
-    print(f"The total bill is: ${bill}")
+    print("Sorry you have to grow taller before you can ride.")
---
+++ Project/water_park.py
@@ -0,0 +1,19 @@
+# Question:
+# Write a Python program that checks if a person is eligible to enter a water park. The program should:
+#
+# Ask the user to input their age.
+# Determine the ticket price based on their age:
+# Ages below 5: Not eligible to enter.
+# Ages 5–12: Ticket price is $8.
+# Ages 13–18: Ticket price is $10.
+# Ages 19–60: Ticket price is $15.
+# Ages 61 and above: Ticket price is $5.
+# Ask the user if they want to rent a locker (additional $2).
+# Calculate and display the total bill.
+
+print("Welcome to the Run Water Park!")
+age = int(input("Please enter your Age: "))
+
+bill = 0
+if age < 5:
+    bill += 8
