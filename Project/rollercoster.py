f0722312395c0bd972e6449d2e64de276090d7e3
sohebshaikh <winsoheb@gmail.com>
2024-12-25 12:33:25 +0530

    new file

--- Project/rollercoster.py
+++ Project/rollercoster.py
@@ -3,22 +3,21 @@ print("Welcome to rollercoster Ride")
 height = int(input("Please enter your height in CM: "))
 age = int(input("Please enter your Age: "))
 photo = input("Do you wnat Photos  Y or N: ").lower()
-canride = False
+
 bill = 0
 if height > 120:
-    canride = True
     print("You can ride")
     if age < 12:
-        bill += 5
+        bill = 5
     elif age > 12 and age < 19:
-        bill += 7
+        bill = 7
     elif age > 18 and age < 45:
-        bill += 12
+        bill = 12
     elif age > 44 and age < 56:
-        bill += 0
+        bill = 0
 else:
     print("You can't ride")
-if photo == "Y" and canride == True:
+if photo == "y":
     bill += 3
     print(f"The totle bill is: ${bill}")
 else:
